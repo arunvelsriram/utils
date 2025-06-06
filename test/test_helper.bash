7f8386fd0d0d2156149dd4909206c5b2741f5ad1
Arunvel Sriram <6568319+arunvelsriram@users.noreply.github.com>
2025-06-06 13:06:36 +0530

    Add brew bats lib path only if brew is installed

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,4 +1,5 @@
-export BATS_LIB_PATH="${BATS_LIB_PATH}:$(brew --prefix)/lib"
+type brew &>/dev/null && export BATS_LIB_PATH="${BATS_LIB_PATH}:$(brew --prefix)/lib"
+
 bats_load_library bats-support
 bats_load_library bats-assert
 
